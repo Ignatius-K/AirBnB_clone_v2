d35262ea3b4dd40ca74c6eaaf45f0d8c3a08b557
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-08-11 14:33:27 +0300

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,39 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Start Flask Application
+
+Script starts a Flask Application
+listening on 0.0.0.0:5000
+
+Routes:
+    - /
+    - /hbnb: Courtesy for Holberton school of Magic
+    - /c: Courtesy for C being cool
+
+Notes:
+    - strict_slashes: set to False
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''Index route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''HBNB route'''
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_route(text: str):
+    '''C route'''
+    return f'C {text.replace("_", " ")}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
