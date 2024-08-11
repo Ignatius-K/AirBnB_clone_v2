a3653f63604666ce9c18573f738999802f173242
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-08-11 14:20:13 +0300

    1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,32 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Start Flask Application
+
+Script starts a Flask Application
+listening on 0.0.0.0:5000
+
+Routes:
+    - /
+    - /hbnb: Courtesy for Holberton school of Magic
+
+Notes:
+    - strict_slashes: set to False
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''Index route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''HBNB route'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
